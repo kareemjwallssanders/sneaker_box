4487735f20b9d0fbfff5972ac1ece505b5438634
KareemjWall-Sanders <kareemjwallssanders@gmail.com>
2019-04-18 21:40:58 -0400

    first push

--- sneakerbox_fullstack/app/routes.js
+++ sneakerbox_fullstack/app/routes.js
@@ -1,87 +1,96 @@
-module.exports = function(app, passport, db, multer, ObjectId) {
-
-//---------------------------------------
-// IMAGE CODE
-//---------------------------------------
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+module.exports = function (app, passport, db, multer, ObjectId) {
+  // ---------------------------------------
+  // IMAGE CODE
+  // ---------------------------------------
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
       cb(null, 'public/images/uploads')
-  },
-  filename: function (req, file, cb) {
-      cb(null, file.fieldname + '_' + Date.now() + ".png")
-  }
-})
-var upload = multer({storage: storage})
-//---------------------------------------
-// IMAGE CODE END
-//---------------------------------------
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '_' + Date.now() + '.png')
+    }
+  })
+  var upload = multer({ storage: storage })
+  // ---------------------------------------
+  // IMAGE CODE END
+  // ---------------------------------------
 
-// pages =========================================================================
+  // pages =========================================================================
 
-  app.get('/', function(req, res) {
-    res.render('index.ejs');
-  });
+  app.get('/', function (req, res) {
+    res.render('index.ejs')
+  })
 
   // after the user is redirected to their profile,
   // the user will be able to upload an image and a name
-  app.get('/profile', isLoggedIn, function(req, res) {
-    let query = {}
-    if ('brand' in req.query && req.query.brand !== "None") {
+  app.get('/profile', isLoggedIn, function (req, res) {
+    let query = {username:req.user.local.name}//to takeout username ':' to see all sneakers
+    if ('brand' in req.query && req.query.brand !== 'None') {
       query['brand'] = req.query.brand
     }
-    db.collection('sneakerapp').find(query).toArray((err, result) => {
-      // console.log(result); getting sneaker data to the terminal
+    db.collection('sneakerapp')
+      .find(query)
+      .toArray((err, result) => {
+        // console.log(result); getting sneaker data to the terminal
 
-      if (err) return console.log(err)
-      res.render('profile.ejs', {
-        sneakers: result,
-        user: req.user
+        if (err) return console.log(err)
+        res.render('profile.ejs', {
+          sneakers: result,
+          user: req.user
+        })
       })
-    })
-    
-  });
+  })
 
-  
-// api ===========================================================================
+  // api ===========================================================================
 
   app.post('/api/profile', upload.array('images', 5), (req, res) => {
     var images = req.files.map(file => '/images/uploads/' + file.filename)
-    db.collection('sneakerapp').save({username: req.body.username, images: images, review: req.body.review}, (err, result) => {
-      if (err) return console.log(err)
-      res.redirect('/profile')
-    })
+    db.collection('sneakerapp').save(
+      { username: req.body.username, images: images, review: req.body.review },
+      (err, result) => {
+        if (err) return console.log(err)
+        res.redirect('/profile')
+      }
+    )
   })
 
   app.put('/api/profile_pic', upload.single('avatar'), (req, res) => {
-    db.collection('users').findOneAndUpdate({_id: ObjectId(req.body._id)}, {
-      $set: {
-        "local.avatar": '/images/uploads/' + req.file.filename
+    db.collection('users').findOneAndUpdate(
+      { _id: ObjectId(req.body._id) },
+      {
+        $set: {
+          'local.avatar': '/images/uploads/' + req.file.filename
+        }
+      },
+      {
+        sort: { _id: -1 },
+        upsert: false
+      },
+      (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
       }
-    }, {
-      sort: {_id: -1},
-      upsert: false
-    }, (err, result) => {
-      if (err) return res.send(err)
-      res.send(result)
-    })
+    )
   })
 
   app.delete('/api/profile', (req, res) => {
-    db.collection('sneakerapp').findOneAndDelete({_id: ObjectId(req.body._id)}, (err, result) => {
-      if (err) return console.log(err)
-      res.send(result)
-    })
-  });  
+    db.collection('sneakerapp').findOneAndDelete(
+      { _id: ObjectId(req.body._id) },
+      (err, result) => {
+        if (err) return console.log(err)
+        res.send(result)
+      }
+    )
+  })
 
-  app.get('/api/sneaker/:id',  function(req, res) {
-    const id = req.params.id;
-    console.log(id);
+  app.get('/api/sneaker/:id', function (req, res) {
+    const id = req.params.id
+    console.log(id)
 
     // figure out how to get a sneaker by id from mongo
     // make template (ejs file) for displaying sneaker data
     // res.render the template with the sneaker data you get
 
-    
     // db.collection('sneakerapp').find().toArray((err, reviews) => {
     //   if (err) return console.log(err)
     //   res.render('profile.ejs', {
@@ -89,44 +98,119 @@ var upload = multer({storage: storage})
     //     user: req.user
     //   })
     // })
-  });
+  })
 
-// =============================================================================
-// login/signup ================================================================
-// =============================================================================
+  app.post('/api/sneakersave', (req, res) => {
+    const doc=[]
+    for (var i = 0; i < req.body.stockxCode.length; i++) {
+      if (req.body.save[i] === 'on') {
+        doc[i]={
+          username: req.user.local.name,
+          name: req.body.name[i],
+          gender: req.body.gender[i],
+          retail_price: req.body.retail_price[i],
+          highest_bid: req.body.highest_bid[i],
+          lowest_ask: req.body.lowest_ask[i],
+          last_sold: req.body.last_sold[i]
 
-  app.get('/login', function(req, res) {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
-  });
+        }
+        
+      }
+      console.log(req.body.stockxCode[i], req.body.save[i])
+    }
+    db.collection('sneakerapp').insert(doc, 
+          
+      (err, result) => {
+        if (err) return console.log(err)
+        res.redirect('/profile')
+      }
+    )
+  })
 
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect : '/profile',
-    failureRedirect : '/login',
-    failureFlash : true
-  }));
+  app.post('/api/deletesneaker',  (req, res) => {
+    db.collection('sneakerapp').deleteOne(
+      { username: req.user.local.name, name: req.body.name},
+      (err, result) => {
+        if (err) return console.log(err)
+        res.redirect('/profile')
+      }
+    )
+  })
 
-  app.get('/signup', function(req, res) {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
-  });
+  app.post('/api/search', (req, res) => {
+    const fetch = require('node-fetch')
 
-  app.post('/signup', upload.single('avatar'), passport.authenticate('local-signup', {
-    successRedirect : '/profile',
-    failureRedirect : '/signup',
-    failureFlash : true
-  }));
+    const query = req.body.query
+    fetch('https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query', {
+      method: 'POST',
+      headers: {
+        'x-algolia-agent': 'Algolia for vanilla JavaScript 3.22.1',
+        'x-algolia-application-id': 'XW7SBCT9V6',
+        'x-algolia-api-key': '6bfb5abee4dcd8cea8f0ca1ca085c2b3'
+        // "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        params: 'query=' + query + '&hitsPerPage=20&facets=*'
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        for (let i = 0; i < json.hits.length; i++) {
+          console.log('HIT #', i)
+          console.log(json.hits[i])
+        }
+        res.render('search.ejs', { sneakers: json })
+      })
+  })
+  app.get('/search', function (req, res) {
+    res.render('search.ejs', {
+      user: req.user,
+      sneakers: null
+    })
+  })
 
-  app.get('/unlink/local', isLoggedIn, function(req, res) {
-    var user            = req.user;
-    user.local.email    = undefined;
-    user.local.password = undefined;
-    user.save(function(err) {
-      res.redirect('/index');
-    });
-  });
+  // =============================================================================
+  // login/signup ================================================================
+  // =============================================================================
 
-  function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-      return next();
-    res.redirect('/');
+  app.get('/login', function (req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') })
+  })
+
+  app.post(
+    '/login',
+    passport.authenticate('local-login', {
+      successRedirect: '/profile',
+      failureRedirect: '/login',
+      failureFlash: true
+    })
+  )
+
+  app.get('/signup', function (req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') })
+  })
+
+  app.post(
+    '/signup',
+    upload.single('avatar'),
+    passport.authenticate('local-signup', {
+      successRedirect: '/profile',
+      failureRedirect: '/signup',
+      failureFlash: true
+    })
+  )
+
+  app.get('/unlink/local', isLoggedIn, function (req, res) {
+    var user = req.user
+    user.local.email = undefined
+    user.local.password = undefined
+    user.save(function (err) {
+      res.redirect('/index')
+    })
+  })
+
+  function isLoggedIn (req, res, next) {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/')
   }
 }
--- sneakerbox_fullstack/public/js/main.js
+++ sneakerbox_fullstack/public/js/main.js
@@ -6,7 +6,6 @@ updateAvatar.addEventListener('click', () => {
     var id = document.querySelector('#userId')
 
 
-
     var data = new FormData()
     data.append('avatar', avatar.files[0])
     data.append('_id', id.value )
@@ -28,11 +27,11 @@ document.getElementById('userInput').addEventListener('click', ()=>{
     window.location = window.location.href.split('?')[0] + "?brand=" + brand
 });
 
-document.getElementById('').addEventListener('click', ()=>{
+document.getElementById('userInput').addEventListener('click', ()=>{
     
     //value that they put in for the select 
 
-    const brand= document.getElementById('brandselect').value
+    const brand= document.getElementById('*').value
     console.log(brand);
     window.location = window.location.href.split('?')[0] + "?brand=" + brand
 });
@@ -41,138 +40,101 @@ document.getElementById('').addEventListener('click', ()=>{
 
 
 //functions for sneaker searcher bar or dropdown....
-// var objSelect = document.getElementById("");
-// setSelectedValue(objSelect, "10");
-// function setSelectedValue(selectObj, valueToSet) {
-//     for (var i = 0; i < selectObj.options.length; i++) {
-//         if (selectObj.options[i].text== valueToSet) {
-//             selectObj.options[i].selected = true;
-//             return;
-//         }
-//     }
-// }
+var objSelect = document.getElementById("brandselect");
+setSelectedValue(objSelect, "10");
+function setSelectedValue(selectObj, valueToSet) {
+    for (var i = 0; i < selectObj.options.length; i++) {
+        if (selectObj.options[i].text== valueToSet) {
+            selectObj.options[i].selected = true;
+            return;
+        }
+    }
+}
 
+////users profile stage===============================================////
 
-// function collapseDropdown() {
-//     $('.expand-icon').removeClass('expanded');
-//     $('.item-container').css('margin-bottom', '');
-//     $('.item-container').removeClass('active');
-//     $('.dropdown-content').addClass('hideContent');
-//     $('.arrow-up').hide();
-// }
+document.getElementById('close-btn').addEventListener('click', function(){
+    $( window ).resize(function() {
+        $('.item-container.active').click();
+      });
 
-// $('.close-btn').on('click keypress', function() {
-//   collapseDropdown();
-// });
-
-//   $('.item-container').on('click keypress', function(e) {
-//     e.preventDefault();
-      
-//     $('.item-container').attr('data-show', 'false');
-//     $(this).attr('data-show', 'true');
+    function collapseDropdown() {
+        $('.expand-icon').removeClass('expanded');
+        $('.item-container').css('margin-bottom', '');
+        $('.item-container').removeClass('active');
+        $('.dropdown-content').addClass('hideContent');
+        $('.arrow-up').hide();
+        console.log(collapseDropdown);
+    }
     
-//     setTimeout(function() {
-//       $('.close-btn').focus();
-//     });
+    $('.close-btn').on('click keypress', function() {
+      collapseDropdown();
+    });
     
-//     $('.expand-icon').removeClass('expanded');
-//     $(this).find('.expand-icon').addClass('expanded');
-    
-//     var dropdownHeight = $(this).next().height() + 180;
-//     $('.item-container').css('margin-bottom', '');
-    
-//     $(this).css('margin-bottom', dropdownHeight);
-      
-//     var x = $(this).position();
-//     var offsetNumber = x.top + $(this).height() + 25;
-    
-//     $(this).next('.dropdown-content').css('top', offsetNumber + "px");
-     
-//     $('.arrow-up').hide();
-//     $(this).find('.arrow-up').show();
-    
-//     $('.item-container').removeClass('active');
-//     $(this).addClass('active');
-
-//     $('.dropdown-content').addClass('hideContent');
-//     $(this).next('.dropdown-content').removeClass('hideContent');
- 
-// });
-
-
-
-// // tabbed js
-
-
-// $('.tab').each(function() {
-//   var clickedTab = this;
-//   $(clickedTab).on('click', function(e) {
-//     e.preventDefault();
-//     collapseDropdown(); 
-//     $('.no-results-header').hide();
-//     var clickedTabAttr = $(this).attr('data-category');
-//     $('.item-container').each(function() {
-//       var targetPanel = this;
-//       var targetPanelAttr = $(this).attr('data-category');
-//       if(clickedTabAttr == 'all') {
-//         $(targetPanel).removeClass('hide');
-//       } else if(clickedTabAttr == targetPanelAttr) {
-//           $(targetPanel).removeClass('hide');
-//       } else {
-//           $(targetPanel).addClass('hide');
-//       }
-//     });
-//   });
-// });
-
-// // date picker js
-
-// $( function() {
-//     $( "#datepicker-to" ).datepicker({
-//         dateFormat: 'mm/dd/yy'
-//     }).datepicker("setDate", new Date());
-//     $( "#datepicker-from" ).datepicker({
-//         dateFormat: 'mm/dd/yy'
-//     }).datepicker("setDate", new Date());;
-//   } );
-  
-
-// $('.apply-cta').click(function(e){
-//     e.preventDefault();
-//     collapseDropdown(); 
-//     $('.no-results-header').hide();
-//     $('.item-container').removeClass('hide');
-    
-  
-//   var dateFrom = $('#datepicker-from').val().split('/').reverse(); // create date format eg 20182911 - yyyy/dd/mm
-//   var dateTo = $('#datepicker-to').val().split('/').reverse();
-  
-//  dateTo.push(dateTo.splice(1, 1)[0]); // put the day of the selected date to the end eg 20181129 - yyyy/mm/dd
-//  dateFrom.push(dateFrom.splice(1, 1)[0]);
-  
-  
-//   $('.item-container').each(function() {
-    
-//         if( $(this).attr('data-start') <= dateTo.join('') && $(this).attr('data-max') >= dateFrom.join('')) {
+      $('.item-container').on('click keypress', function(e) {
+        e.preventDefault();
+          
+        $('.item-container').attr('data-show', 'false');
+        $(this).attr('data-show', 'true');
         
-//             $(this).removeClass('hide');
+        setTimeout(function() {
+          $('.close-btn').focus();
+        });
+        
+        $('.expand-icon').removeClass('expanded');
+        $(this).find('.expand-icon').addClass('expanded');
+        
+        var dropdownHeight = $(this).next().height() + 180;
+        $('.item-container').css('margin-bottom', '');
+        
+        $(this).css('margin-bottom', dropdownHeight);
           
-//         } else {
-          
-//             $(this).addClass('hide');
-//         }
-                
-//     });
+        var x = $(this).position();
+        var offsetNumber = x.top + $(this).height() + 25;
+        
+        $(this).next('.dropdown-content').css('top', offsetNumber + "px");
+         
+        $('.arrow-up').hide();
+        $(this).find('.arrow-up').show();
+        
+        $('.item-container').removeClass('active');
+        $(this).addClass('active');
     
-  
-// /***********************
+        $('.dropdown-content').addClass('hideContent');
+        $(this).next('.dropdown-content').removeClass('hideContent');
+     
+    });
+    
+    
+    
+    // tabbed js
+    
+    
+    $('.tab').each(function() {
+      var clickedTab = this;
+      $(clickedTab).on('click', function(e) {
+        e.preventDefault();
+        collapseDropdown(); 
+        $('.no-results-header').hide();
+        var clickedTabAttr = $(this).attr('data-category');
+        $('.item-container').each(function() {
+          var targetPanel = this;
+          var targetPanelAttr = $(this).attr('data-category');
+          if(clickedTabAttr == 'all') {
+            $(targetPanel).removeClass('hide');
+          } else if(clickedTabAttr == targetPanelAttr) {
+              $(targetPanel).removeClass('hide');
+          } else {
+              $(targetPanel).addClass('hide');
+          }
+        });
+      });
+    });
+    
+    
 
-// If no dates match the tiles   
-// s
-// ***********************/
-    
-//    if ($('.item-container.hide').length === $('.max-width-container .item-container').length) {
-//        $('.max-width-container').after('<h1 class="no-results-header" style="text-align:center;">No Results Found</h1>');
-//     } 
-    
-// });
+
+
+});
+
+////users profile stage===============================================////
